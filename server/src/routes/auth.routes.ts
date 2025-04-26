f012fd24d5aba62393c152d0cf21d55c06ca9df1
Asad Shafiq <Masadali5433@gmail.com>
2025-04-27 01:52:11 +0500

    add endpoint to get my details

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -3,12 +3,16 @@ import {
   initiateRedditAuth,
   handleRedditCallback,
   devLogin,
+  getMyDetails,
 } from "../controllers/auth.controller";
+import { requireAuth } from "../middleware/auth.middleware";
 
 const router = Router();
 
 router.get("/dev-login", devLogin);
 router.get("/reddit", initiateRedditAuth);
 router.get("/reddit/callback", handleRedditCallback);
+
+router.get("/reddit/me", requireAuth, getMyDetails);
 
 export default router;
